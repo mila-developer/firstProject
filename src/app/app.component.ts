7ab88b417ce8c992293bbd6f1adeae8635448bee
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2024-06-04 10:21:03 +0100

    add task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
@@ -10,11 +12,11 @@ import { TasksComponent } from "./tasks/tasks.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent]
+    imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf]
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUsersId = 'u1';
+  selectedUsersId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUsersId);
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,41 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
-  selector: 'app-tasks',
-  standalone: true,
-  imports: [],
-  templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+    selector: 'app-tasks',
+    standalone: true,
+    templateUrl: './tasks.component.html',
+    styleUrl: './tasks.component.css',
+    imports: [TaskComponent]
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) name!: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced concepts of Angular',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31'
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue templte',
+      summary: 'Prepare and describe an issue template which will help projetc management',
+      dueDate: '2024-06-15'
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
